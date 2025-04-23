177740f3b981db9a4a9972c0485750e77d81c68e
Harshit Raj <harshit@50-50-52-210.snpr.wi.frontiernet.net>
2025-04-23 13:09:06 +0530

    new_commit1

--- my-app1/src/pages/index.js
+++ my-app1/src/pages/index.js
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <>
-    <h1>Business - 2</h1>
+    <h1>HERBAT TEA</h1>
     <img src = "https://imgs.search.brave.com/YN5yAVhaKs5A9kj1ZjTcL8ufcU3GvqRbB68C8msk_Y0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9oZW1wLXByb2R1/Y3RzLWNvbmNlcHQt/Y2FubmFiaXMtc2Vl/ZC1vaWwtc2tlaW4t/Z3JlZW4tcGxhbnQt/d2hpdGUtYmFja2dy/b3VuZF8zMjM2MzQt/NjQ1LmpwZz9zZW10/PWFpc19oeWJyaWQm/dz03NDA" alt ="img"></img>
     </>
   );
